82b84aa10b34a20a3becd36419d141e7804ff41e
biedrekGit <128526304+biedrekGit@users.noreply.github.com>
2025-02-15 17:00:35 +0100

    7.2 added packages: iterate, immutable

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,12 +1,14 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 public class StreamMain {
 
     public static void main(String[] args) {
+        //chapter 7.1
         ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 
         System.out.println("Calculating expressions with lambdas");
@@ -35,6 +37,9 @@ public class StreamMain {
         poemBeautifier.beautify("StringSix", (string -> string.substring(3)));
         poemBeautifier.beautify("StringSeven", (string -> string.replaceAll("S", "XXX")));
 
-
+        //chapter 7.2
+        System.out.println();
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
